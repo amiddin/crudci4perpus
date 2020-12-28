71186036bb587e9e1b08c889f3bde995068ae494
Ahmad Muhyiddin <44324964+amiddin@users.noreply.github.com>
2020-12-28 09:57:19 +0700

    fiks template

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -13,23 +13,23 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="/assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="/assets/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="/assets/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="https://crudci4perpus.herokuapp.com/assets/plugins/summernote/summernote-bs4.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -64,7 +64,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="<?php echo base_url(); ?>/assets/dist/img/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="<?php echo base_url(); ?>https://crudci4perpus.herokuapp.com/assets/dist/img/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="#" class="d-block">ADMIN</a>
@@ -126,39 +126,39 @@
   <!-- ./wrapper -->
 
   <!-- jQuery -->
-  <script src="/assets/plugins/jquery/jquery.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/jquery/jquery.min.js"></script>
   <!-- jQuery UI 1.11.4 -->
-  <script src="/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
     $.widget.bridge('uibutton', $.ui.button)
   </script>
   <!-- Bootstrap 4 -->
-  <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
   <!-- ChartJS -->
-  <script src="/assets/plugins/chart.js/Chart.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/chart.js/Chart.min.js"></script>
   <!-- Sparkline -->
-  <script src="/assets/plugins/sparklines/sparkline.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/sparklines/sparkline.js"></script>
   <!-- JQVMap -->
-  <script src="/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-  <script src="/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
   <!-- jQuery Knob Chart -->
-  <script src="/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
   <!-- daterangepicker -->
-  <script src="/assets/plugins/moment/moment.min.js"></script>
-  <script src="/assets/plugins/daterangepicker/daterangepicker.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/moment/moment.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/daterangepicker/daterangepicker.js"></script>
   <!-- Tempusdominus Bootstrap 4 -->
-  <script src="/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
   <!-- Summernote -->
-  <script src="/assets/plugins/summernote/summernote-bs4.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/summernote/summernote-bs4.min.js"></script>
   <!-- overlayScrollbars -->
-  <script src="/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
   <!-- AdminLTE App -->
-  <script src="/assets/dist/js/adminlte.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/dist/js/adminlte.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <script src="/assets/dist/js/pages/dashboard.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/dist/js/pages/dashboard.js"></script>
   <!-- AdminLTE for demo purposes -->
-  <script src="/assets/dist/js/demo.js"></script>
+  <script src="https://crudci4perpus.herokuapp.com/assets/dist/js/demo.js"></script>
 </body>
 
 </html>
