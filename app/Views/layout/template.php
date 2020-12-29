a0f82a2e4b5260115e015e67279113722413e33c
Ahmad Muhyiddin <44324964+amiddin@users.noreply.github.com>
2020-12-29 14:05:52 +0700

    fiks yuk

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -15,7 +15,7 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
