65c5f3bca602b1c27f48a80ca0d30ee8fcc68123
Ahmad Muhyiddin <44324964+amiddin@users.noreply.github.com>
2020-12-29 14:11:22 +0700

    fiks profile

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -50,7 +50,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="/assets/dist/img/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="/assets/profile.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="#" class="d-block">ADMIN</a>
