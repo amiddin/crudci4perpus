c7f1dc29589e6522abc5a001073efa8336582b09
Ahmad Muhyiddin <44324964+amiddin@users.noreply.github.com>
2020-12-29 13:54:30 +0700

    fiks semoga

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -5,8 +5,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?= $title; ?></title>
-  <!-- Ilustrasi dari freepik -->
-  <link rel="" href="https://www.freepik.com/vectors/health">
   <!-- My Font -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Inria+Sans:wght@700&display=swap" rel="stylesheet">
@@ -16,20 +14,8 @@
   <link rel="stylesheet" href="/assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="/assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="/assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="/assets/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <!-- <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css"> -->
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="/assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="/assets/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="/assets/plugins/summernote/summernote-bs4.css">
+  <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
@@ -64,7 +50,7 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-
+            <img src="/assets/dist/img/user8-128x128.jpg" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
             <a href="#" class="d-block">ADMIN</a>
@@ -127,38 +113,14 @@
 
   <!-- jQuery -->
   <script src="/assets/plugins/jquery/jquery.min.js"></script>
-  <!-- jQuery UI 1.11.4 -->
-  <script src="/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
   <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
   <script>
     $.widget.bridge('uibutton', $.ui.button)
   </script>
   <!-- Bootstrap 4 -->
   <script src="/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- ChartJS -->
-  <script src="/assets/plugins/chart.js/Chart.min.js"></script>
-  <!-- Sparkline -->
-  <script src="/assets/plugins/sparklines/sparkline.js"></script>
-  <!-- JQVMap -->
-  <script src="/assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-  <script src="/assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-  <!-- jQuery Knob Chart -->
-  <script src="/assets/plugins/jquery-knob/jquery.knob.min.js"></script>
-  <!-- daterangepicker -->
-  <script src="/assets/plugins/moment/moment.min.js"></script>
-  <script src="/assets/plugins/daterangepicker/daterangepicker.js"></script>
-  <!-- Tempusdominus Bootstrap 4 -->
-  <script src="/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-  <!-- Summernote -->
-  <script src="/assets/plugins/summernote/summernote-bs4.min.js"></script>
-  <!-- overlayScrollbars -->
-  <script src="/assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
   <!-- AdminLTE App -->
-  <!-- <script src="/assets/dist/js/adminlte.js"></script> -->
-  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-  <!-- <script src="/assets/dist/js/pages/dashboard.js"></script> -->
-  <!-- AdminLTE for demo purposes -->
-  <!-- <script src="/assets/dist/js/demo.js"></script> -->
+  <script src="/assets/dist/js/adminlte.js"></script>
 </body>
 
 </html>
